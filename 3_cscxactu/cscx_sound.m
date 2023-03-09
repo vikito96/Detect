dbbb68cbbcf0bf4d4825736075a704282dcbb03e
vikito96 <tongweiqi@zju.edu.cn>
2023-03-09 16:44:46 +0800

    sound函数更新停止功能

--- 3_cscxactu/cscx_sound.m
+++ 3_cscxactu/cscx_sound.m
@@ -1,5 +1,9 @@
 function cscx_sound(hObject,~,x,Fs)
-if get(hObject,'Value')==1
+if strcmp(get(hObject,'string'),'播放')
+    set(hObject,'String','停止');
     sound(x,Fs);
+elseif strcmp(get(hObject,'string'),'停止')
+    set(hObject,'String','播放');
+    clear sound
 end
 end
